2dc938b9d6c25c992e0b3f5d06658365e9b50f43
minji <62049151+meanz1@users.noreply.github.com>
2023-09-03 20:44:40 +0900

    [Silver IV] Title: 수 찾기, Time: 172 ms, Memory: 50404 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = set(map(int, input().split()))
+m = int(input())
+arr = list(map(int, input().split()))
+
+for num in arr:
+  if num in a:
+    print(1)
+  else:
+    print(0)
