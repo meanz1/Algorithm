656a537770fa2a9fcacce43c0ab33905d2b0b033
minji <62049151+meanz1@users.noreply.github.com>
2023-09-03 20:06:44 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int stop;
+	int people;
+	int on;
+	int off;
+	int out;
+
+	cin >> stop >> people;
+	for (int i = 0; i < stop; i++) {
+		cin >> on >> off;
+		
+		
+
+	
+	}
+	cout << "비와이";
+}
