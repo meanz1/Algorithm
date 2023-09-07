b2e3be4dc6f6b5906bcfa34ff5f899ad08b65c9e
minji <62049151+meanz1@users.noreply.github.com>
2023-09-07 16:24:57 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,14 @@
+import sys
+n = int(sys.stdin.readline())
+
+cache = [0]*1001
+
+cache[1] = 1
+cache[2] = 3
+
+if n >=3:
+  for i in range(3, n+1):
+    cache[i] = (cache[i-1] + 2*cache[i-2])%10007
+
+print(cache[n])
+
