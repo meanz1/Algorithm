e72d548370473358535a189a19f67d0fbb5a43a4
minji <62049151+meanz1@users.noreply.github.com>
2023-10-07 00:20:41 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cpp
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 string solution(string my_string, int n) {
     string answer = "";
-    reverse(my_string.begin(), my_string.end());
-    answer = my_string.substr(0, n);
-    reverse(answer.begin(), answer.end());
+
+    answer = my_string.substr(my_string.length()-n);
+
     return answer;
 }
