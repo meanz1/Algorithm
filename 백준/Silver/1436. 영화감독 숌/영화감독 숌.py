a739f434a5521d085be77f23c342d3e72a15a3b0
minji <62049151+meanz1@users.noreply.github.com>
2023-10-24 00:34:33 +0900

    [Silver V] Title: 영화감독 숌, Time: 604 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+num = 665
+
+while True:
+  num+=1
+  if '666' in str(num):
+    cnt +=1
+    if cnt == n:
+      print(num)
+      break
