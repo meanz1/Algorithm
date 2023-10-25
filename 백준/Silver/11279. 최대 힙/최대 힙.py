868cc9755634674ab6d188fbf860efd713cd7439
minji <62049151+meanz1@users.noreply.github.com>
2023-10-25 23:20:55 +0900

    [Silver II] Title: 최대 힙, Time: 128 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+hq = []
+n = int(input())
+
+for _ in range(n):
+  temp = int(sys.stdin.readline())
+  if temp == 0:
+    if len(hq):
+      print(-heapq.heappop(hq))
+    else:
+      print(0)
+  else:
+    heapq.heappush(hq, -temp)
