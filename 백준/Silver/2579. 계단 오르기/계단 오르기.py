87852e94ac88a8d121b02aa243fd0cd3bbf4f3b8
minji <62049151+meanz1@users.noreply.github.com>
2024-01-11 21:31:54 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,16 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
+stairs = [
+  int(input()) for _ in range(n)
+]
 
-s = [ int(input()) for _ in range(n)]
-
-dp = [0] * n
-if len(s) <= 2:
-    print(sum(s))
+dp = [0]*(n)
+if n <= 2:
+  print(sum(stairs))
 else:
-    dp[0] = s[0]
-    dp[1] = s[0] + s[1]
-    
-    for i in range(2, n):
-        dp[i] = max(dp[i-3]+s[i-1]+s[i], dp[i-2]+s[i])
-
-    print(dp[n-1])
-        
+  dp[0] = stairs[0]
+  dp[1] = stairs[0]+stairs[1]
+  for i in range(2, n):
+    dp[i] = max(dp[i-2]+stairs[i], dp[i-3]+stairs[i-1]+stairs[i])
+  print(dp[n-1])
