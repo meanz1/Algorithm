be8f081b95e7fa1ab4c8be9f0b1c5948fab95388
minji <62049151+meanz1@users.noreply.github.com>
2024-01-18 23:34:13 +0900

    [Gold III] Title: 아기 상어, Time: 132 ms, Memory: 34132 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,55 +1,73 @@
 from collections import deque
+
 n = int(input())
-map = [list(map(int, input().split())) for _ in range(n)]
-shark_x, shark_y = 0, 0
-dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-time = 0
-shark_size = 2
-ate = 0
+grid = [
+  list(map(int, input().split())) for _ in range(n)
+]
+
+q = deque() # 먹이 찾을 때 BFS할 큐 만듦
+
+dxs, dys = [-1, 0, 0, 1], [0, -1, 1, 0] # 상 좌 우 하 순으로
+
+cx, cy = 0, 0 # 상어의 현재 위치
+time = 0 # 최종 반환할 시간탐색
+size = 2 # 상어의 초기 크기
+must_eat = 0 # 성장을 위해 꼭 먹어야하는 물고기 수
+
 for i in range(n):
-    for j in range(n):
-        if map[i][j] == 9:
-            shark_x, shark_y = i, j
-            break
-def shark_search(shark_x, shark_y):
-    global time
-    global shark_size
-    global ate
-    while True:
-        cand = BFS(shark_x, shark_y)
-        if not cand:
-            break
-        time += cand[0]
-        map[cand[1]][cand[2]] = 0 
-        shark_x, shark_y = cand[1], cand[2]
-        ate+=1        
-        if shark_size == ate:
-            shark_size+=1
-            ate = 0    
-    return time
-def BFS(sx, sy):
-    global shark_size
-    q = deque()
-    q.append((0, sx, sy)) #거리 행 열 순으로 정렬할거니까. 
-    map[sx][sy] = 0
-    food=[]
-    visited = [[False] * n for _ in range(n)]
-    visited[sx][sy] = True
-    while q:    
-        cur_d, cur_x, cur_y = q.popleft()       
-        for x, y in dir:
-            nx = cur_x + x
-            ny = cur_y + y            
-            if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
-                if 0<=map[nx][ny]<=shark_size:
-                    visited[nx][ny] = True
-                    n_d =cur_d+1
-                    q.append((n_d, nx, ny))
-                if map[nx][ny] !=0 and map[nx][ny] < shark_size:
-                    food.append((n_d, nx, ny))                    
-    if food:
-        food.sort()
-        return food[0] 
-    else:
-        return  
-print(shark_search(shark_x, shark_y))
+  for j in range(n):
+    if grid[i][j] == 9:
+      cx, cy = i, j # 상어의 현재 위치 설정
+      break
+
+def in_range(x, y):
+  return 0<=x<n and 0<=y<n
+
+
+def bfs(cz, cx, cy):
+  visited = [
+    [False]*n for _ in range(n)
+  ]
+  q.append((cz, cx, cy))
+  visited[cx][cy] = True
+  result = []
+  while q:
+    z, x, y = q.popleft()
+
+    for dx, dy in zip(dxs, dys):
+      nx, ny = x+dx, y+dy
+      if(in_range(nx, ny) and not visited[nx][ny]):
+        if(0 == grid[nx][ny] or grid[nx][ny] == size): # 지나갈 수만 있음
+          q.append((z+1, nx, ny))
+          visited[nx][ny] = True
+          
+        elif ( 0 < grid[nx][ny] < size): # 실질적으로 먹을 수 있는 것
+          result.append((z+1, nx, ny))
+          
+        
+  result.sort()
+  if(len(result) > 0):
+    return result[0]
+  return 0;
+
+
+def find_fish():
+  global time, must_eat, size, grid
+  global cx, cy
+  while True:
+    cand = bfs(0, cx, cy)
+    grid[cx][cy] = 0
+
+    if not cand: break # 후보 없으면 break
+
+    time += cand[0]
+    cx = cand[1]
+    cy = cand[2]
+
+    must_eat +=1
+    if (must_eat == size):
+      size += 1
+      must_eat = 0
+  return time
+
+print(find_fish())
