a39a1ccf939a06901ef73f161cf86fe5c6b81b0c
minji <62049151+meanz1@users.noreply.github.com>
2024-02-01 21:30:44 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 437 ms, Memory: 96,956 KB -BaekjoonHub

---
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+class Solution
+{
+	static int N, M;
+    static int[] snacks, selected;
+    static int weight;
+
+    static void combination(int start, int cnt){
+        if(cnt==2){
+            int tempWeight = Arrays.stream(selected).sum();
+            if(tempWeight <= M){
+                weight = Math.max(weight, tempWeight);
+            }
+            return;
+        }
+        for(int i=start; i<N; i++){
+            selected[cnt] = snacks[i];
+            combination(i+1, cnt+1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(st.nextToken());
+        for(int t = 1; t<= T; t++){
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            weight = -1;
+            snacks = new int[N];
+            selected = new int[2];
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++){
+                snacks[i] = Integer.parseInt(st.nextToken());
+            }
+            combination(0, 0);
+            sb.append("#"+t+" "+weight+"\n");
+        }
+        System.out.println(sb);
+    }
+}
